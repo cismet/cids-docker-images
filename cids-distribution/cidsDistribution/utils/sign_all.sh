9a6fde5d0e89753cf0419e3f8d67709c12a5a6b9
p-a-s-c-a-l <pascal@cismet.de>
2016-12-07 19:12:54 +0100

    #9 sign_all script improved

--- cids-distribution/cidsDistribution/utils/sign_all.sh
+++ cids-distribution/cidsDistribution/utils/sign_all.sh
@@ -3,19 +3,20 @@
 set -e
 
 # sign only files that have been modified after last call to sign_all!
-if [[ ! -f ${CIDS_LIB_DIR}/.signed ]]; then
-    touch -t 197001010000.00 ${CIDS_LIB_DIR}/.signed 
+if [[ ! -f ${MAVEN_LIB_DIR}/.signed ]]; then
+    touch -t 197001010000.00 ${MAVEN_LIB_DIR}/.signed 
 fi
 
 # keystore deleted in image after build!
 if [[ -f ${CIDS_DISTRIBUTION_DIR}/.private/keystore && ${CIDS_DISTRIBUTION_DIR}/.private/keystore.pwd ]]; then
-    last_modified=$(stat -c %y "${CIDS_LIB_DIR}/.signed")
-    echo -e "\e[32mINFO\e[39m: Signing all JAR Files in \e[1m${CIDS_LIB_DIR}\e[0m that have been modified since $last_modified"
+    last_modified=$(stat -c %y "${MAVEN_LIB_DIR}/.signed")
+    echo -e "\e[32mINFO\e[39m: Checking signatures of all JAR Files in \e[1m${MAVEN_LIB_DIR}\e[0m that have been modified since $last_modified"
+    find -L ${MAVEN_LIB_DIR} -name *.jar -type f -newermm ${MAVEN_LIB_DIR}/.signed
     # don't sign maven plugins (excluded via path, list is incomplate!)
-    find -L ${CIDS_LIB_DIR} -name *.jar -type f -not -path "${MAVEN_LIB_DIR}/org/apache/maven/*" -not -path "${MAVEN_LIB_DIR}/org/sonatype/*" -not -path "${MAVEN_LIB_DIR}/org/codehaus/plexus/*" -newermm ${CIDS_LIB_DIR}/.signed -exec ${CIDS_DISTRIBUTION_DIR}/utils/sign.sh ${CIDS_DISTRIBUTION_DIR}/.private/keystore `cat ${CIDS_DISTRIBUTION_DIR}/.private/keystore.pwd` {} \;
-    touch ${CIDS_LIB_DIR}/.signed
+    find -L ${MAVEN_LIB_DIR} -name *.jar -type f -not -path "${MAVEN_LIB_DIR}/org/apache/maven/*" -not -path "${MAVEN_LIB_DIR}/org/sonatype/*" -not -path "${MAVEN_LIB_DIR}/org/codehaus/plexus/*" -newermm ${MAVEN_LIB_DIR}/.signed -exec ${CIDS_DISTRIBUTION_DIR}/utils/sign.sh ${CIDS_DISTRIBUTION_DIR}/.private/keystore `cat ${CIDS_DISTRIBUTION_DIR}/.private/keystore.pwd` {} \;
+    touch ${MAVEN_LIB_DIR}/.signed
 else
-    echo -e "\e[31mERROR\e[39m: Could not sign all JAR files in \e[1m${CIDS_LIB_DIR}\e[0m, keystore not available"
+    echo -e "\e[31mERROR\e[39m: Could not check signatures of all JAR files in \e[1m${MAVEN_LIB_DIR}\e[0m, keystore not available"
     # hard fail
     exit 1
 fi
