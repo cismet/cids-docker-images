80287ab37bd77c8443063f503d87a3afb46e78b7
p-a-s-c-a-l <pascal@cismet.de>
2017-02-07 10:55:06 +0100

    minor fix in build script

--- cids-server/build.sh
+++ cids-server/build.sh
@@ -4,4 +4,4 @@
 # docker build -t cismet/cids-server:latest -t cismet/cids-server:4.0 .
 
 # SNAPSHOT BUILD ---------------------------------------------------------------
-docker build -t cismet/cids-server:latest-snapshot -t cismet/cids-server-rest-legacy:4.1-SNAPSHOT .
+docker build -t cismet/cids-server:latest-snapshot -t cismet/cids-server:4.1-SNAPSHOT .
