10fb08ab8d28d3a5df4276c64a2339e65575df1f
p-a-s-c-a-l <pascal@cismet.de>
2017-03-28 16:17:40 +0200

    #14 update cids-server-rest-legacy to v5.0

--- cids-server/build.sh
+++ cids-server/build.sh
@@ -4,4 +4,4 @@
 docker build -t cismet/cids-server:latest -t cismet/cids-server:5.0 .
 
 # SNAPSHOT BUILD ---------------------------------------------------------------
-#docker build -t cismet/cids-server:latest-snapshot -t cismet/cids-server:5.0-SNAPSHOT .
+#docker build -t cismet/cids-server:latest-snapshot -t cismet/cids-server:5.1-SNAPSHOT .
