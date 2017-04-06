2efa250a5ad0c65d1c95593668ca2fd6bac56dd5
p-a-s-c-a-l <pascal@cismet.de>
2017-04-06 15:39:14 +0200

    #14 upgrade cids-server and cids-server-rest-legacy images to new cids-distribution

--- cids-integration-base/build.sh
+++ cids-integration-base/build.sh
@@ -3,5 +3,5 @@
 # RELEASE BUILD ----------------------------------------------------------------
 docker build -t cismet/cids-integration-base:latest -t cismet/cids-integration-base:postgres-9.6.1-2.0 .
 
-SNAPSHOT BUILD ---------------------------------------------------------------
-docker build -t cismet/cids-integration-base:latest-snapshot -t cismet/cids-integration-base:postgres-9.6.1-2.0-SNAPSHOT .
+#SNAPSHOT BUILD ---------------------------------------------------------------
+#docker build -t cismet/cids-integration-base:latest-snapshot -t cismet/cids-integration-base:postgres-9.6.1-2.0-SNAPSHOT .
--- cids-java-maven/build.sh
+++ cids-java-maven/build.sh
@@ -3,5 +3,5 @@
 # RELEASE BUILD ----------------------------------------------------------------
 docker build -t cismet/cids-java-maven:jdk-1.8u121-1.0 .
 
-SNAPSHOT BUILD ---------------------------------------------------------------
+# SNAPSHOT BUILD ---------------------------------------------------------------
 #docker build -t cismet/cids-java-maven:latest-snapshot -t cismet/cids-java-maven:jdk-1.8u121-1.0-SNAPSHOT .
